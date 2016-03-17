e752bd6a03aaa83d2c0141b4e324b4472a70106e
jrsouth <james.south@gmail.com>
2016-03-17 13:38:09 +0000

    Added default settings file

---
+++ settings-template.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$db_server = 'localhost:3306';
+$db_database = 'dbname';
+$db_user = 'dbuser';
+$db_password = 'dbpass';
+
+
+
+$settings['columns'] = 8;
+$settings['columns-public'] = 8;
+$settings['columns-tablet'] = 5;
+$settings['columns-update'] = 7;
+
+
+$settings['refresh-rate'] = 60; // Number of seconds between updates (0 = never)
+$settings['refresh-rate-public'] = 15; // Number of seconds between updates
+$settings['refresh-rate-tablet'] = 120; // Number of seconds between updates
+$settings['refresh-rate-update'] = 3000; // Number of seconds between updates
